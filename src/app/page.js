cb93fc1375ec2739aa934cd063830b59b0a0a31f
klee0126@student.monash.edu <klee0126@student.monash.edu>
2025-05-31 18:46:20 +1000

    feat: generalised stylings for sections

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
 import Header from "@/components/header"
+import RippleBackground from "@/components/ripple-background"
+import WorkSection from "@/components/work-section"
+import AboutSection from "@/components/about-section"
 
 export default function HomePage() {
     return (
-      <main className="studio-page">
-        <Header />
-      </main>
+        <main className="studio-page">
+            <Header />
+            <RippleBackground />
+            <AboutSection />
+            <WorkSection />
+        </main>
     )
 }
