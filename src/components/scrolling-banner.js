de9be39d74b7fd47199acd032224987a1225f30d
klee0126@student.monash.edu <klee0126@student.monash.edu>
2025-06-05 13:45:00 +1000

    feat: implemented a scorlling banner component

---
+++ src/components/scrolling-banner.js
@@ -0,0 +1,40 @@
+"use client"
+
+import styles from "@/styles/scrolling-banner.module.css"
+
+export default function ScrollingBanner() {
+    const services = [
+        "Conversion Optimisation",
+        "E-Commerce Development",
+        "Shopify Migration",
+        "UI & UX Design",
+        "Brand Strategy",
+        "Digital Marketing",
+        "SEO Optimisation",
+        "Social Media Management",
+        "Content Creation",
+        "Analytics & Reporting",
+    ]
+
+    return (
+        <div className={styles.scrollingBanner}>
+            <div className={styles.scrollingContent}>
+                
+                {/* first set of items */}
+                {services.map((service, index) => (
+                    <span key={`first-${index}`} className={styles.scrollingItem}>
+                        {service}
+                    </span>
+                ))}
+                
+                {/* duplicate for loop */}
+                {services.map((service, index) => (
+                    <span key={`second-${index}`} className={styles.scrollingItem}>
+                        {service}
+                    </span>
+                ))}
+
+            </div>
+        </div>
+    )
+}
