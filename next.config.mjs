4041859663f2055778704b55385b9877369c7637
klee0126@student.monash.edu <klee0126@student.monash.edu>
2025-06-17 19:39:20 +1000

    fix: fix asset paths for gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  basePath: "/dg-landing",
+  assetPrefix: "/dg-landing/",
+};
 
 export default nextConfig;
