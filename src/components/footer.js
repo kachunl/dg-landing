6c46c056c17f9f8f138034eedb19c0aba5487b99
klee0126@student.monash.edu <klee0126@student.monash.edu>
2025-06-17 22:53:32 +1000

    fix: added id for footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import styles from "@/styles/footer.module.css"
 
 export default function Footer() {
     return (
-        <footer className={styles.footer}>
+        <footer id="footer" className={styles.footer}>
             <div className={styles.footerInner}>
                 <div className={styles.footerContainer}>
                     <div className={styles.footerContent}>
