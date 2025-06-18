cd15884901e20d3accb819b8ad9b5068a03d14aa
klee0126@student.monash.edu <klee0126@student.monash.edu>
2025-06-18 19:53:37 +1000

    fix: removed base path from config

--- next.config.mjs
+++ next.config.mjs
@@ -13,11 +13,6 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-
-    ...(process.env.NODE_ENV === "production" && {
-        basePath: "/dg-landing",
-        assetPrefix: "/dg-landing/",
-    }),
 };
 
 export default nextConfig;
